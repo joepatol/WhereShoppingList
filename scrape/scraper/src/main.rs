65aa6ec68951520a68c7342e444c9fa65beaccf2
Joep Atol <jatol@ilionx.com>
2024-01-26 13:52:37 +0100

    bit of reorganizing

--- scrape/scraper/src/main.rs
+++ scrape/scraper/src/main.rs
@@ -1,40 +1,20 @@
 mod funcs;
 mod response;
+mod state;
 
-use std::sync::Arc;
 use log::{info, LevelFilter};
+use serde_json::json;
 use simple_logger::SimpleLogger;
+use warp::{Filter, Rejection, Reply, http::Response};
 use funcs::scrape;
 use scrape_core::connectors::get_html_document_from_url;
 use scrape_core::ConfigBuilder;
-use tokio::sync::Mutex;
+use crate::state::StateKeeper;
 use crate::response::{ScraperState, ScraperStateResponse};
-use warp::{Filter, Rejection, Reply, http::Response};
 
 const HOST: [u8; 4] = [0, 0, 0, 0];
 const PORT: u16 = 7071;
 type Result<T> = std::result::Result<T, Rejection>;
-
-#[derive(Clone)]
-struct StateKeeper {
-    state: Arc<Mutex<ScraperState>>,
-}
-
-impl StateKeeper {
-    pub fn new() -> Self {
-        StateKeeper { state: Arc::new(Mutex::new(ScraperState::Idle)) }
-    }
-
-    pub async fn change_state(&self, new_state: ScraperState) {
-        let mut state = self.state.lock().await;
-        *state = new_state;
-    }
-
-    pub async fn get_state(&self) -> ScraperState {
-        let state = self.state.lock().await;
-        state.clone()
-    }
-}
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
@@ -42,14 +22,16 @@ async fn main() {
         .map(|()| log::set_max_level(LevelFilter::Info))
         .expect("Failed to initialize logger");
 
-    let state_keeper = StateKeeper::new();
+    let state_keeper = StateKeeper::new(ScraperState::Idle);
     let status_clone = state_keeper.clone();
     let func_clone = state_keeper.clone();
 
-    let health_check_route = warp::get()
+    let health_check_route = 
+        warp::get()
         .and(warp::path("health_check"))
         .and_then(health_check);
-    let scrape_route = warp::get()
+    let scrape_route = 
+        warp::post()
         .and(warp::path("scrape_func"))
         .map(move || func_clone.clone())
         .map(|state_clone| {
@@ -59,7 +41,8 @@ async fn main() {
             let scraper_state = ScraperStateResponse::new(ScraperState::Started);
             serde_json::to_string(&scraper_state).unwrap()
         });
-    let status_route = warp::get()
+    let status_route = 
+        warp::get()
         .and(warp::path("status"))
         .map(move || status_clone.clone())
         .and_then(get_handler_state);
@@ -75,16 +58,21 @@ async fn main() {
 }
 
 async fn health_check() -> Result<impl Reply> {
-    Ok(Response::builder().body("Hi, all is looking dandy!"))
+    Ok(Response::builder()
+        .body(
+            serde_json::to_string(&json!({"state": "ok"}))
+            .unwrap()
+        )
+    )
 }
 
-async fn get_handler_state(state_keeper: StateKeeper) -> Result<impl Reply>  {
+async fn get_handler_state(state_keeper: StateKeeper<ScraperState>) -> Result<impl Reply>  {
     let scraper_state = state_keeper.get_state().await;
     let response = ScraperStateResponse::new(scraper_state);
     Ok(Response::builder().body(serde_json::to_string(&response).unwrap()))
 }
 
-async fn handler(state_keeper: StateKeeper) {
+async fn handler(state_keeper: StateKeeper<ScraperState>) {
     if state_keeper.get_state().await == ScraperState::Running {
         return
     }
